8ad9e5b43aa0ef860abce47c0318e2c9cf189a33
wesley <wesley_snaine@hotmail.com>
2023-05-05 00:14:10 -0300

    add a knight icon for knight's position

--- src/modules/node.js
+++ src/modules/node.js
@@ -17,11 +17,20 @@ class Node {
             this.#hasKnight = true;
             this.square.removeAttribute('id'); // remove the target attribute
             this.square.setAttribute('id', 'knight');
+            this.square.appendChild(
+                new Range().createContextualFragment(
+                    `<svg class="knight-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <title>Knight</title>
+                        <path d="M20 8V16L17 17L13.91 11.5C13.65 11.04 12.92 11.27 13 11.81L14 21L4 17L5.15 8.94C5.64 5.53 8.56 3 12 3H20L18.42 5.37C19.36 5.88 20 6.86 20 8Z" />
+                    </svg>`
+                )
+            );
         }
         // Case where knight is removed from here
         else {
             this.#hasKnight = false;
             this.square.removeAttribute('id');
+            this.square.removeChild(this.square.querySelector('.knight-icon'));
         }
     }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: {
         index: './src/index.js',
     },
